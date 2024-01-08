592e71f6e148400697e879c0f02fbbdf3ac6b95a
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2024-01-07 23:14:19 -0300

    primer commit

---
+++ lib/request.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest,NextApiResponse } from "next"
+
+export function getOffseAndLimit(req: NextApiRequest, maxLimit, maxOffset) {
+    const queryLimit = parseInt(req.query.limit as string)
+    const queryOffset = parseInt(req.query.offset as string)
+    const limit = queryLimit <= maxLimit ? queryLimit : maxLimit
+    const offset = queryOffset < maxOffset? queryOffset : 0
+    console.log("soy el offset",offset)
+    return { limit, offset }
+}
